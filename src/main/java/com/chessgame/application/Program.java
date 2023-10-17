c6311f507913dc7d1f78b71a2fd6937830fc4847
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2023-10-17 00:11:07 -0300

    started implementation of `Board` and `Piece`

--- src/main/java/com/chessgame/application/Program.java
+++ src/main/java/com/chessgame/application/Program.java
@@ -1,12 +1,12 @@
 package com.chessgame.application;
 
+import com.chessgame.boardgame.Board;
 import com.chessgame.boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-        Position position = new Position(1,2);
-        System.out.println(position);
+        Board board = new Board(8,8);
+
     }
 }
