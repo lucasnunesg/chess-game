de2f40a45bbab315762a2a78b9718be96b7fd421
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2023-10-17 00:00:18 -0300

    created `Position` class

--- src/main/java/com/chessgame/application/Program.java
+++ src/main/java/com/chessgame/application/Program.java
@@ -1,8 +1,12 @@
 package com.chessgame.application;
 
+import com.chessgame.boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!")
+        System.out.println("Hello World!");
+        Position position = new Position(1,2);
+        System.out.println(position);
     }
 }
